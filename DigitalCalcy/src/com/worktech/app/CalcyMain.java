f359d18466cd5849c34f0542e1516ef83054afba
ramaki5 <rk.mundru@gmail.com>
2019-09-19 01:10:15 +0530

    Calcy Operations..

--- DigitalCalcy/src/com/worktech/app/CalcyMain.java
+++ DigitalCalcy/src/com/worktech/app/CalcyMain.java
@@ -4,6 +4,10 @@ public class CalcyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CalcyOperation calcyOperation = new CalcyOperation();
+		calcyOperation.add();
+		calcyOperation.divsion();
+		calcyOperation.multiplay();
 		System.out.println("My Application Started...");
 	}
 
---
+++ DigitalCalcy/src/com/worktech/app/CalcyOperation.java
@@ -0,0 +1,20 @@
+package com.worktech.app;
+
+public class CalcyOperation {
+
+	public void add() {
+
+	}
+
+	public void multiplay() {
+
+	}
+
+	public void sub() {
+
+	}
+
+	public void divsion() {
+
+	}
+}
