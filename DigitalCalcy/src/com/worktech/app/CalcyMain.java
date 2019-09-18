b5a0d887cba1c54ad76e24572cb88fa94cc18acc
ramaki5 <rk.mundru@gmail.com>
2019-09-19 01:02:35 +0530

    Calcy Main Changes..

---
+++ DigitalCalcy/src/com/worktech/app/CalcyMain.java
@@ -0,0 +1,10 @@
+package com.worktech.app;
+
+public class CalcyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Application Started...");
+	}
+
+}
